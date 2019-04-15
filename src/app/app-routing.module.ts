ac9b5826e04b3f2e65dceb9af1cfc54cbc6b19ef
bryceohmer <bryce.ohmer@brunswickcountync.gov>
2019-04-15 14:59:02 -0400

    starting on landing page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { RouterModule } from '@angular/router';
+import { HomeRoutingModule } from './home/home-routing.module';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [HomeRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
